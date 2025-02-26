2be2d63ed6d0f3fb7ecb438261fab8aa5f6e37dd
BaldySam <147501901+BaldySam@users.noreply.github.com>
2025-02-26 19:33:24 +0000

    Car balance changes
    Made the car feel more balanced

--- Sustainability Game Jam/Assets/CameraLook.cs
+++ Sustainability Game Jam/Assets/CameraLook.cs
@@ -2,18 +2,36 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour
 {
-    [SerializeField] private float sensitivity = 100f;
-    [SerializeField] private Camera playerCamera;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Mouse")]
+    [SerializeField] private float mouseX;
+    [SerializeField] private float mouseY;
+
+    [Header("Camera")]
+    [SerializeField] private GameObject playerHead;
+    [SerializeField] private Vector2 xClamp;
+    [SerializeField] private Vector2 yClamp;
+
+    [Header("Camera Sensitivity")]
+    [SerializeField] private float sensitivity;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerCamera.transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime);
-        playerCamera.transform.Rotate(Vector3.left * Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime);
+        RotateCam();
+    }
+
+    void RotateCam()
+    {
+        mouseX += Input.GetAxis("Mouse X") * sensitivity;
+        mouseY -= Input.GetAxis("Mouse Y") * sensitivity;
+        mouseY = Mathf.Clamp(mouseY, yClamp.x, yClamp.y);
+        mouseX = Mathf.Clamp(mouseX, xClamp.x, xClamp.y);
+        playerHead.transform.localRotation = Quaternion.Euler(mouseY, mouseX, 0);
     }
 }
